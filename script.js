95e07231d7d91baae57981802d3f0f49b24ea07a
Wixp <wisxpeet@gmail.com>
2025-06-23 05:34:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const atomic = new Audio("i-am-atomic-resound.mp3")
 const button1 = document.getElementById("btn1")
+const btn = document.getElementById("btn")
 let isPlaying = false;
 button1.addEventListener("click", function(clicked) {
 if (!isPlaying) {
@@ -10,4 +11,12 @@ if (!isPlaying) {
       atomic.pause();
       isPlaying = false;
     }
+})
+
+btn.addEventListener("click", function() {
+      window.open("https://pypi.org/project/wispylib/");
+      console.log("https://pypi.org/project/wispylib/")
+      window.open("https://github.com/Wixp-dc")
+      audio.play()
+      
 })
